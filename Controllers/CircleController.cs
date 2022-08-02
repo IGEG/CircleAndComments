ae739ba77d22588ffe5a90bfcc2384578f71732c
igor egorov <igor127@bk.ru>
2022-08-02 22:07:18 +0300

    all

--- Controllers/CircleController.cs
+++ Controllers/CircleController.cs
@@ -7,28 +7,29 @@ namespace CircleAndComments.Controllers
 {
     public class CircleController : Controller
     {
-       
+
         private readonly IDataCircle dataCircle;
-        public CircleController( IDataCircle _dataCircle)
+        public CircleController(IDataCircle _dataCircle)
         {
             this.dataCircle = _dataCircle;
         }
-        
-        public  IActionResult Index()
+
+        public IActionResult Index()
         {
             return View();
         }
 
         [HttpGet]
         public async Task<JsonResult> GetAllCirclesAndComments()
-        { 
-        return await dataCircle.GetAllCirclesAsync();  
+        {
+            return await dataCircle.GetAllCirclesAsync();
         }
 
-        [HttpDelete]
-        public async Task DeleteCirclesAndComments(int Id)
+        [HttpDelete("{Id}")]
+        public async Task<IActionResult> DeleteCirclesAndComments(int Id)
         {
             await dataCircle.DeleteCircleAsync(Id);
+            return View(nameof(Index));
              
         }
 
